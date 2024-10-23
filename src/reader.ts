12c215dca8616c98ba26aae091aa614c89453f78
荀洪道 <kalxd@126.com>
2024-10-23 20:27:17 +0800

    初版的Reader

--- src/reader.ts
+++ src/reader.ts
@@ -1,5 +1,3 @@
-import { EitherAsync, Right, Left } from "purify-ts";
-
 export interface Reader<T, R> {
 	runReader: (env: Readonly<T>) => R;
 	pipe: <RA>(reader: Reader<R, RA>) => Reader<T, RA>;
@@ -9,6 +7,7 @@ export interface ReaderCtx<T> {
 	ask: () => Readonly<T>;
 	asks: <R>(f: (env: Readonly<T>) => R) => R;
 	bindFrom: <RA>(reader: Reader<T, RA>) => RA;
+	withBind: <TA, R>(r: Reader<TA, R>, f: (env: T) => TA) => R;
 }
 
 export const reader = <T, R>(f: (helper: ReaderCtx<T>) => R): Reader<T, R> => {
@@ -16,7 +15,8 @@ export const reader = <T, R>(f: (helper: ReaderCtx<T>) => R): Reader<T, R> => {
 		const ctx: ReaderCtx<T> = {
 			ask: () => env,
 			asks: g => g(env),
-			bindFrom: r => r.runReader(env)
+			bindFrom: r => r.runReader(env),
+			withBind: (r, f) => r.runReader(f(env))
 		};
 
 		return f(ctx);
@@ -32,57 +32,3 @@ export const reader = <T, R>(f: (helper: ReaderCtx<T>) => R): Reader<T, R> => {
 		pipe
 	};
 };
-
-export interface Handler<T, E, R> extends Reader<T, EitherAsync<E, R>> {
-	bindPipe: <RA>(handler: Handler<R, E, RA>) => Handler<T, E, RA>;
-}
-
-export interface HandlerCtx<T> extends ReaderCtx<T> {
-	asksN: () => number;
-}
-
-export const mkHandler = <T, E, R>(f: (ctx: HandlerCtx<T>) => EitherAsync<E, R>): Handler<T, E, R> => {
-	const theReader = reader((ctx: ReaderCtx<T>) => {
-		const asksN: HandlerCtx<T>["asksN"] = () => 10;
-		return f({ ...ctx, asksN });
-	});
-
-	const bindPipe = <RA>(handle: Handler<R, E, RA>): Handler<T, E, RA> => {
-		return mkHandler(ctx => {
-			return f(ctx).chain(handle.runReader);
-		});
-	};
-
-	return {
-		...theReader,
-		bindPipe
-	}
-};
-
-
-/// examples
-const inc: Handler<number, string, number> = mkHandler(ctx => {
-	const n = ctx.ask();
-	if (n > 5) {
-		return EitherAsync.liftEither(Left("more than 5"));
-	}
-	else {
-		return EitherAsync.liftEither(Right(ctx.asksN() + 1));
-	}
-});
-
-const f = inc.bindPipe(inc).bindPipe(inc).bindPipe(inc).bindPipe(inc).bindPipe(inc);
-
-const main = async (): Promise<void> => {
-	const v = await f.runReader(1);
-	v.caseOf({
-		Right: a => {
-			console.log("ok", a);
-		},
-		Left: e => {
-			console.error("err", e);
-		}
-	})
-};
-
-main().catch(console.error);
