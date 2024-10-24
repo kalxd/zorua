71208a4e572fa5cc8872948dce868390686c7050
荀洪道 <kalxd@126.com>
2024-10-24 19:09:48 +0800

    基础的服务框架。

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,31 @@
+import { createServer, IncomingMessage } from "node:http";
+
+import { Reader, ReaderCtx, reader } from "./reader";
 import { EitherAsync, Right } from "purify-ts";
 
-const readFile = (): EitherAsync<string, string> => {
-	return EitherAsync.liftEither(Right(""));
+interface HttpState<S> {
+	req: IncomingMessage;
+	state: S;
+}
+
+interface HttpApplication {
+	listen: (port: number, callback: () => void) => void;
+}
+
+const application = <S>(state: S): HttpApplication => {
+	const listen: HttpApplication["listen"] = (port, callback) => {
+		const srv = createServer((_, res) => {
+			res.end("ok");
+		});
+
+		srv.listen(port, undefined, undefined, callback);
+	};
+
+	return {
+		listen
+	};
 };
+
+
+application(undefined)
+	.listen(3000, () => console.log("start!"));
